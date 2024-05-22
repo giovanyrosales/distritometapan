605a6a0dd305111274a00740bda82b973b45235d
tatangames <54828743+tatangames@users.noreply.github.com>
2024-05-22 09:53:45 -0600

    estilo front v1

--- resources/views/frontend/menu/indexsuperior.blade.php
+++ resources/views/frontend/menu/indexsuperior.blade.php
@@ -1,77 +1,78 @@
 <!DOCTYPE HTML>
 <html class="scrollbar-deep-purple thin square">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Alcaldía Metapán</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="" />
-	<meta name="keywords" content="" />
-	<meta name="author" content="" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Alcald&iacute;a Metap&aacute;n</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
 
-	<link href="{{ asset('images/LOGO_2_-_copia.png') }}" rel='shortcut icon' type='image/png'>
-	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
-	<link href="{{ asset('flaticon/font/flaticon.css') }}" type="text/css" rel="stylesheet" />
+    <link href="{{ asset('images/LOGO_2_-_copia.png') }}" rel='shortcut icon' type='image/png'>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+    <link href="{{ asset('flaticon/font/flaticon.css') }}" type="text/css" rel="stylesheet" />
     <link href="{{ asset('css/frontend.css') }}" type="text/css" rel="stylesheet" />
-	<!-- <link rel="stylesheet" href="{{asset('fonts/icomoon/icomoon.ttf')}}"> -->
+    <!-- <link rel="stylesheet" href="{{asset('fonts/icomoon/icomoon.ttf')}}"> -->
 
-	<link href="{{ asset('plugins/animaciones/animaciones.css') }}" type="text/css" rel="stylesheet" />
-
-
-	<script src="{{ asset('js/jquery.min.js') }}" type="text/javascript"></script>
+    <link href="{{ asset('plugins/animaciones/animaciones.css') }}" type="text/css" rel="stylesheet" />
 
 
-	<script src="{{ asset('js/modernizr-2.6.2.min.js') }}" type="text/javascript"></script>
-	<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <script src="{{ asset('js/jquery.min.js') }}" type="text/javascript"></script>
 
-	@yield('header')
-	<style>
-		/* On screens that are 768px or less, set the logo bigger */
-		@media screen and (max-width: 768px) {
-  			.logoimage {
-				   width: 170px;
-				   height: 50px;
-				   margin-bottom: -5px;
-  					}
-				}
-			/* Para scroll */
-			.scrollbar-deep-purple::-webkit-scrollbar-track {
-			-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
-			background-color: #F5F5F5;
-			border-radius: 10px; }
 
-			.scrollbar-deep-purple::-webkit-scrollbar {
-			width: 12px;
-			background-color: #F5F5F5; }
+    <script src="{{ asset('js/modernizr-2.6.2.min.js') }}" type="text/javascript"></script>
+    <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 
-			.scrollbar-deep-purple::-webkit-scrollbar-thumb {
-			border-radius: 10px;
-			-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
-			background-color: #04742c; }
+    @yield('header')
+    <style>
+        /* On screens that are 768px or less, set the logo bigger */
+        @media screen and (max-width: 768px) {
+            .logoimage {
+                width: 170px;
+                height: 50px;
+                margin-bottom: -5px;
+            }
+        }
+        /* Para scroll */
+        .scrollbar-deep-purple::-webkit-scrollbar-track {
+            -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+            background-color: #F2F2F2;
+            border-radius: 10px; }
 
-			.square::-webkit-scrollbar-track {
-			border-radius: 0 !important; }
+        .scrollbar-deep-purple::-webkit-scrollbar {
+            width: 12px;
+            background-color: #F2F2F2; }
 
-			.square::-webkit-scrollbar-thumb {
-			border-radius: 0 !important; }
+        .scrollbar-deep-purple::-webkit-scrollbar-thumb {
+            border-radius: 10px;
+            -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+            background-color: #213F59; }
 
-			.thin::-webkit-scrollbar {
-			width: 6px; }
-			#soc {
-				border-radius: 5px;
-  				border: 1px solid rgb(238,238,238);
-  				padding: 7px;
-				  width: 125px;
-				  color:rgb(238,238,238);
-				  font-size:1.5em;
-			}
-			#soc .p {color:rgb(238,238,238);
-				  font-size:1.5em;}
-			#soc:hover {
-				background-color:rgb(238,238,238);
-				color:#04742c;
-			}
-			@media screen and (max-width: 768px) {
-			  #socno { display:none; }}
+        .square::-webkit-scrollbar-track {
+            border-radius: 0 !important; }
+
+        .square::-webkit-scrollbar-thumb {
+            border-radius: 0 !important; }
+
+        .thin::-webkit-scrollbar {
+            width: 6px; }
+        #soc {
+            border-radius: 5px;
+            border: 1px solid rgb(238,238,238);
+            padding: 7px;
+            width: 125px;
+            color:rgb(238,238,238);
+            font-size:1.5em;
+        }
+        #soc .p {color:rgb(238,238,238);
+            font-size:1.5em;}
+        #soc:hover {
+            background-color:rgb(238,238,238);
+            color:#10ACFA;
+        }
+        @media screen and (max-width: 768px) {
+            #socno { display:none; }}
     </style>
 </head>
--- resources/views/frontend/menu/navbar.blade.php
+++ resources/views/frontend/menu/navbar.blade.php
@@ -1,158 +1,142 @@
-    <nav class="colorlib-nav" role="navigation">
-    	<div class="top-menu" style="background-color: rgb(238,238,238); height: 60px;">
-    		<div class="container-fluid ">
-    			<div class="row">
-    				<div class="col-xs-3">
-    					<div id="colorlib-logo"><a href="/"><img class="logoimage" src="{{ asset('images/LogoWeb.png') }}" alt="Alcaldia Municipal de Metapan" width="35%" height="10%" style="margin-top: -42px"></div>
-    				</div>
-    				<div class="col-xs-9 text-right menu-1">
-    					<ul>
-    						<li class="active"><strong><a href="/">Inicio</a></strong></li>
-							<!-- Pestaña para revista
+<nav class="colorlib-nav" role="navigation">
+    <div class="top-menu" style="background-color: #F2F2F2; height: 60px;">
+        <div class="container-fluid ">
+            <div class="row">
+                <div class="col-xs-3">
+                    <div id="colorlib-logo"><a href="/"><img class="logoimage" src="{{ asset('images/HPNG.png') }}" alt="Alcaldia Municipal de Metapan" width="35%" height="10%" style="margin-top: -42px"></div>
+                </div>
+                <div class="col-xs-9 text-left menu-1">
+                    <ul style="width: 100%; margin-bottom: 5%;">
+                        <li class="active"><strong><a href="/">Inicio</a></strong></li>
+                        <!-- Pestaña para revista
 							<li><strong><a href="{{ url('https://metapanfiestas.alcaldiademetapan.gob.sv/index.html') }}" target="_blank">Revista</a></strong></li>
- -->
+							fin pesta;a revista -->
+                        <li><strong><a href="{{ url('noticias/') }}">Noticias</a></strong></li>
+                        <li class="has-dropdown"><strong>
+                                <a >Servicios</a></strong>
+                            <ul class="dropdown">
+                                @foreach($serviciosMenu as $dato3)
+                                    <li><strong><a href="{{ url('servicio/'.$dato3->slug) }}">{{$dato3->nombreservicio}}</a></strong></li>
+                                @endforeach
+                                <li><strong><a href="{{ url('servicios/') }}">Ver todos</a></strong></li>
 
-                            <!-- fin pesta;a revista -->
-    						<li><strong><a href="{{ url('noticias/') }}">Noticias</a></strong></li>
-    						<li class="has-dropdown"><strong>
-    							<a >Servicios</a></strong>
-    							<ul class="dropdown">
-    								@foreach($serviciosMenu as $dato3)
-    								<li><strong><a href="{{ url('servicio/'.$dato3->slug) }}">{{$dato3->nombreservicio}}</a></strong></li>
-									@endforeach
-									<li><strong><a href="{{ url('servicios/') }}">Ver todos</a></strong></li>
+                                <li><strong><a href="#" onclick="verReunion();">UCP</a></strong></li>
 
-                                        <li><strong><a href="#" onclick="verReunion();">UCP</a></strong></li>
-
-    							</ul>
-    						</li>
-    						<li class="has-dropdown"><strong>
-    							<a>Tu Alcad&iacute;a</a></strong>
-    							<ul class="dropdown">
-								<li><strong><a href="{{ url('programas/') }}">Programas Municipales</a></strong></li>
-									<li><strong><a href="{{ url('direccion/') }}">Gobierno municipal</a></strong></li>
-    								<li><strong><a href="{{ url('historia/') }}">Historia</a></strong></li>
-    							</ul>
-    						</li>
-    						<li><strong><a href="{{ url('galeria/') }}">Galer&iacute;a</a></strong></li>
-                            <li><strong><a href="{{ url('finanzas/') }}">Finanzas</a></strong></li>
-                            <li><strong><a href="{{ url('contravencional/') }}">Unidad Contravencional</a></strong></li>
-							<li><strong><a href="https://www.transparencia.gob.sv/institutions/alc-metapan">Transparencia</a></strong></li>
-                            <li><strong><a href="{{ route('compras.publicas') }}">Compras</a></strong></li>
-    					</ul>
-    				</div>
-    			</div>
-    		</div>
-    	</div>
-    </nav>
-
-
-
-    <div class="modal fade" id="modalUCP">
-        <div class="modal-dialog" style="max-width: 800px">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="tituloucp" style="font-size: 16px"></h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form id="formulario-ucp">
-                        <div class="card-body">
-
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-12">
-
-                                        <div class="form-group" style="margin-top: 15px">
-                                            <label id="descripcionlink" style="font-size: 17px"></label>
-                                        </div>
-
-                                        <div class="form-group" style="text-align: center">
-                                            <a style="color: blue; font-size: 18px" href="#" target="_blank" id="myLink"><strong>LINK UCP</strong></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer justify-content-between">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                            </ul>
+                        </li>
+                        <li class="has-dropdown"><strong>
+                                <a>Tu Alcad&iacute;a</a></strong>
+                            <ul class="dropdown">
+                                <li><strong><a href="{{ url('programas/') }}">Programas Municipales</a></strong></li>
+                                <li><strong><a href="{{ url('direccion/') }}">Gobierno municipal</a></strong></li>
+                                <li><strong><a href="{{ url('historia/') }}">Historia</a></strong></li>
+                            </ul>
+                        </li>
+                        <li><strong><a href="{{ url('galeria/') }}">Galer&iacute;a</a></strong></li>
+                        <li><strong><a href="{{ url('finanzas/') }}">Finanzas</a></strong></li>
+                        <li><strong><a href="{{ url('contravencional/') }}">Contravencional</a></strong></li>
+                        <li><strong><a href="https://www.transparencia.gob.sv/institutions/alc-metapan">Transparencia</a></strong></li>
+                        <li><strong><a href="{{ route('compras.publicas') }}">Compras</a></strong></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
+</nav>
 
 
-    <script src="{{ asset('js/sweetalert2.all.min.js') }}"></script>
-    <script src="{{ asset('js/axios.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('js/toastr.min.js') }}" type="text/javascript"></script>
-    <script>
 
-        function verReunion(){
+<div class="modal fade" id="modalUCP">
+    <div class="modal-dialog" style="max-width: 800px">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="tituloucp" style="font-size: 16px"></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="formulario-ucp">
+                    <div class="card-body">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group" style="margin-top: 15px">
+                                        <label id="descripcionlink" style="font-size: 17px"></label>
+                                    </div>
+                                    <div class="form-group" style="text-align: center">
+                                        <a style="color: blue; font-size: 18px" href="#" target="_blank" id="myLink"><strong>LINK UCP</strong></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
 
-            openLoading();
 
-            axios.post('/admin/informacion-ucp', {
-            })
-                .then((response) => {
+<script src="{{ asset('js/sweetalert2.all.min.js') }}"></script>
+<script src="{{ asset('js/axios.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/toastr.min.js') }}" type="text/javascript"></script>
+<script>
 
-                    closeLoading();
+    function verReunion(){
+        openLoading();
+        axios.post('/admin/informacion-ucp', {
+        })
+            .then((response) => {
+                closeLoading();
+                if(response.data.success === 1){
 
-                    if(response.data.success === 1){
-
-                        Swal.fire({
-                            title: 'Link no disponible',
-                            text: "Comunicarse con Jefe de UCP",
-                            icon: 'info',
-                            showCancelButton: false,
-                            allowOutsideClick: false,
-                            confirmButtonColor: '#28a745',
-                            cancelButtonColor: '#d33',
-                            confirmButtonText: 'Aceptar'
-                        }).then((result) => {
-                            if (result.isConfirmed) {
-
-                            }
-                        })
-                    }
-                    if(response.data.success === 2){
-                        abrirPanel(response);
-                    }
-                    else {
-                        toastr.error('Error');
-                    }
-                })
-                .catch((error) => {
-                    closeLoading();
+                    Swal.fire({
+                        title: 'Link no disponible',
+                        text: "Comunicarse con Jefe de UCP",
+                        icon: 'info',
+                        showCancelButton: false,
+                        allowOutsideClick: false,
+                        confirmButtonColor: '#28a745',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Aceptar'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                        }
+                    })
+                }
+                if(response.data.success === 2){
+                    abrirPanel(response);
+                }
+                else {
                     toastr.error('Error');
-                });
-        }
-
-        function abrirPanel(response){
-
-            document.getElementById('tituloucp').innerHTML = response.data.titulo;
-            document.getElementById('descripcionlink').innerHTML = response.data.descripcion;
-
-            document.getElementById("myLink").href= response.data.urllink;
-
-            $('#modalUCP').modal('show');
-        }
-
-        function openLoading(){
-            Swal.fire({
-                heightAuto: false,
-                title: "Cargando..."
+                }
             })
-            Swal.showLoading();
-        }
+            .catch((error) => {
+                closeLoading();
+                toastr.error('Error');
+            });
+    }
 
-        function closeLoading(){
-            Swal.close();
-        }
+    function abrirPanel(response){
+        document.getElementById('tituloucp').innerHTML = response.data.titulo;
+        document.getElementById('descripcionlink').innerHTML = response.data.descripcion;
+        document.getElementById("myLink").href= response.data.urllink;
+        $('#modalUCP').modal('show');
+    }
 
+    function openLoading(){
+        Swal.fire({
+            heightAuto: false,
+            title: "Cargando..."
+        })
+        Swal.showLoading();
+    }
 
-    </script>
+    function closeLoading(){
+        Swal.close();
+    }
+</script>
