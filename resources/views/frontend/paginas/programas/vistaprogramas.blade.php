54361b58f0c0c0218163ce3fe1edd1713b58743e
tatangames <54828743+tatangames@users.noreply.github.com>
2024-08-09 15:14:00 -0600

    portada slider agregad

--- resources/views/frontend/paginas/programas/vistaprogramas.blade.php
+++ resources/views/frontend/paginas/programas/vistaprogramas.blade.php
@@ -11,7 +11,7 @@
     <aside id="colorlib-hero">
         <div class="flexslider">
             <ul class="slides">
-                <li style="background-image: url(images/Slider/Ser_Prog.jpg);">
+                <li style="background-image: url({{ asset('images/Slider/portadaslider.jpg')}});">
                     <div class="overlay"></div>
                     <div class="container-fluid">
                         <div class="row">
