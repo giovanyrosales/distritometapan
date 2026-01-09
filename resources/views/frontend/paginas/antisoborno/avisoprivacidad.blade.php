5a8d4de954aa54bb62a9c68d5179b32b1f3d963b
Tatan <tatangamess@gmail.com>
2026-01-09 08:31:53 -0600

    aviso de privacidad1

--- resources/views/frontend/paginas/antisoborno/avisoprivacidad.blade.php
+++ resources/views/frontend/paginas/antisoborno/avisoprivacidad.blade.php
@@ -172,7 +172,7 @@
                         <div class="row">
                             <div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
                                 <div class="slider-text-inner text-center">
-                                    <h1><strong>POLÍTICA ANTISOBORNO</strong></h1>
+                                    <h1><strong>AVISO DE PRIVACIDAD</strong></h1>
                                 </div>
                             </div>
                         </div>
